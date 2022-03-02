51df9617a6c160f4ddbf407bf74457ebc622e69c
Prachi Nawathe <prachi.nawathe@thoughtworks.com>
2022-03-02 11:52:42 +0530

    Use handle operator

--- src/main/kotlin/com/thoughtworks/reactorexamples/handle/HandleExample.kt
+++ src/main/kotlin/com/thoughtworks/reactorexamples/handle/HandleExample.kt
@@ -1,6 +1,8 @@
 package com.thoughtworks.reactorexamples.handle
 
 import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
+import reactor.core.publisher.SynchronousSink
 
 
 class HandleExample {
@@ -8,8 +10,24 @@ class HandleExample {
     private val alphabet = Alphabet()
 
     fun handle(input: Flux<Int>): Flux<String> {
-        return input.mapNotNull {
-            alphabet.alphabetFor(it)
+        return input.handle { inputValue, sink: SynchronousSink<String> ->
+            val alphabet = alphabet.alphabetFor(inputValue)
+            if(alphabet == null) {
+                println("invalid value : $inputValue")
+            } else {
+                sink.next(alphabet)
+            }
+        }
+    }
+
+    fun handle(input: Mono<Int>): Mono<String> {
+        return input.handle { inputValue, sink: SynchronousSink<String> ->
+            val alphabet = alphabet.alphabetFor(inputValue)
+            if(alphabet == null) {
+                println("invalid value : $inputValue")
+            } else {
+                sink.next(alphabet)
+            }
         }
     }
 }
--- src/test/kotlin/com/thoughtworks/reactorexamples/handle/HandleExampleTest.kt
+++ src/test/kotlin/com/thoughtworks/reactorexamples/handle/HandleExampleTest.kt
@@ -2,6 +2,7 @@ package com.thoughtworks.reactorexamples.handle
 
 import org.junit.jupiter.api.Test
 import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 
 internal class HandleExampleTest {
 
@@ -10,5 +11,10 @@ internal class HandleExampleTest {
     @Test
     fun `should return the Alphabets for given numbers`() {
         handleExample.handle(Flux.just(-1, 30, 13, 9, 20)).subscribe { println(it) }
+    }
+
+    @Test
+    fun `should return the Alphabet for given number`() {
+        handleExample.handle(Mono.just(-1)).subscribe { println(it) }
     }
 }
